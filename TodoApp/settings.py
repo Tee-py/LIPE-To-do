b2229ddff57a50fd29a43631121e271e0abd64d4
Emmanuel Oluwatobi <emmanueloluwatobi2000@gmail.com>
2020-05-11 00:30:45 +0100

    third one

--- TodoApp/settings.py
+++ TodoApp/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 #DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://lipetodoapp.herokuapp.com/']
 
 
 # Application definition
