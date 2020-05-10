dacfae398ffc6de18a038cec8f0d2ad5fe78603a
Emmanuel Oluwatobi <emmanueloluwatobi2000@gmail.com>
2020-05-11 00:42:33 +0100

    Last commit

--- TodoApp/settings.py
+++ TodoApp/settings.py
@@ -40,12 +40,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
     'todo',
     'crispy_forms',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
